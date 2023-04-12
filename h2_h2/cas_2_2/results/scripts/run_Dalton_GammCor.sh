98de8f85d4a10c622d08df42499144b0b46732a9
Michał Hapka <michal.hapka@gmail.com>
2023-04-12 18:56:16 +0200

    mh --added H2-H2 example

---
+++ h2_h2/cas_2_2/results/scripts/run_Dalton_GammCor.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# ************ How to use this script ********
+#  bash <path_to_dalton_executable> <path_to_gammcor_executable> 
+
+DALTON_EXEC=$1
+GAMMCOR_EXEC=$2
+
+#DALTON_EXEC="$HOME/Programs/dalton/build_intel/dalton"
+#GAMMCOR_EXEC="$HOME/Programs/gammcor/build_IntCholesky/gammcor"
+
+# ***********  Aim of the scipt is twofold ***
+# 1) RUN DALTON  : CASSCF
+# 2) RUN GAMMCOR : SAPT
+
+# preliminaries
+
+function grepper {
+local output=$1
+local dist=$2
+
+ elst=$(grep "Eelst"        $output | awk '{print $3}')
+ e1ex=$(grep "E1exch(S2)"   $output | awk '{print $3}')
+ e2ind=$(grep "E2ind "      $output | tail -1 | awk '{print $3}')
+ e2disp=$(grep "E2disp "    $output | tail -1 | awk '{print $3}')
+ e2xi=$(grep "E2exch-ind"   $output | tail -1 | awk '{print $3}')
+ e2xd=$(grep "E2exch-disp " $output | tail -1 | awk '{print $3}')
+ etot=$(grep "Eint(SAPT"    $output | awk '{print $3}')
+
+ echo $dist,$elst,$e1ex,$e2ind,$e2xi,$e2disp,$e2xd,$etot >> res.dat
+
+}
+
+if [ -z $1 ] ; then
+  echo "Error! Please specify path to Dalton executable as the first arg"
+  exit 1
+fi
+
+# end preliminaries
+
+export OMP_NUM_THREADS=1
+
+mkdir -p MONOMER_A
+mkdir -p MONOMER_B
+
+### CALCULATION STARTS HERE
+# ****************************************
+
+for i in 1.0 5.0 ; do
+
+   cp $i'_A.mol'  MONOMER_A/run.mol
+   cp $i'_A.dal'  MONOMER_A/run.dal
+
+   cd MONOMER_A
+   $DALTON_EXEC -noarch -mb 100 -get "AOTWOINT AOONEINT SIRIFC SIRIUS.RST rdm2.dat " run  > skas
+
+   cd ..
+
+   cp $i'_B.mol'  MONOMER_B/run.mol
+   cp $i'_B.dal'  MONOMER_B/run.dal
+
+   cd MONOMER_B
+   $DALTON_EXEC -noarch -mb 100 -get "AOONEINT AMFI_SYMINFO.TXT SIRIFC SIRIUS.RST rdm2.dat " run  > skas
+
+   cd ..
+
+   #MONOMER A
+   mv MONOMER_A/run.AOONEINT    AOONEINT_A
+   mv MONOMER_A/run.AOTWOINT    AOTWOINT_A
+   mv MONOMER_A/run.SIRIFC      SIRIFC_A
+   mv MONOMER_A/run.SIRIUS.RST  SIRIUS_A.RST
+   mv MONOMER_A/run.rdm2.dat    rdm2_A.dat
+
+   #MONOMER_B
+   mv MONOMER_B/run.AOONEINT       AOONEINT_B
+   mv MONOMER_B/run.SIRIFC         SIRIFC_B
+   mv MONOMER_B/run.SIRIUS.RST     SIRIUS_B.RST
+   mv MONOMER_B/run.rdm2.dat       rdm2_B.dat 
+   mv MONOMER_B/run.AMFI_SYMINFO*  SYMINFO_B
+
+   # run GammCor
+   $GAMMCOR_EXEC > "gammcor_$i.out"
+   grepper "gammcor_$i.out"
+
+done
---
+++ h2_h2/cisd/results/scripts/run.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+###### how to use this script #######
+#bash run.sh <path_to_quantum_package.rc> <path_to_gammcor_executable>
+
+# INPUT PARAMETERS 
+QP_RC=$1
+GAMMCOR_EXEC=$2
+
+BASIS=aug-cc-pvdz
+
+#QP_RC="$HOME/Programs/qp2
+#GAMMCOR_EXEC="$HOME/Programs/gammcor/build_IntCholesky/gammcor"
+
+source $QP_RC/quantum_package.rc
+
+###### define functions ###########
+
+function grepper {
+local output=$1
+local dist=$2
+
+ elst=$(grep "Eelst"        $output | awk '{print $3}')
+ e1ex=$(grep "E1exch(S2)"   $output | awk '{print $3}')
+ e2ind=$(grep "E2ind "      $output | tail -1 | awk '{print $3}')
+ e2disp=$(grep "E2disp "    $output | tail -1 | awk '{print $3}')
+ e2xi=$(grep "E2exch-ind"   $output | tail -1 | awk '{print $3}')
+ e2xd=$(grep "E2exch-disp " $output | tail -1 | awk '{print $3}')
+ etot=$(grep "Eint(SAPT"    $output | awk '{print $3}')
+
+ echo $dist,$elst,$e1ex,$e2ind,$e2xi,$e2disp,$e2xd,$etot >> res.dat
+
+}
+
+function input_gammcor {
+local dist=$1
+
+cat << EOF > input.inp
+
+Calculation
+ JobTitle   H2-H2 $i (QPACKAGE)
+ Interface  TREXIO
+ JobType    SAPT
+ RDMType    CI
+ TwoMoInt   FOFO
+ IPrint     11
+end
+
+System
+ Monomer       A
+ NAtoms        2
+ ZNucl         2
+ TrexFile      A.h5
+End
+
+System
+ Monomer       B
+ NAtoms        2
+ ZNucl         2
+ TrexFile      B.h5
+End
+
+EOF
+
+}
+
+###### end functions ###########
+
+if [ -z $1 ] ; then
+  echo "Error! Please specify path to quantum_package.rc file as the first arg"
+  exit 1
+fi
+
+
+# main loop
+for i in 1.0 5.0 ; do
+
+   for m in "A" "B" ; do
+
+      # Create initial EZFIO database
+      qp create_ezfio -a -b $BASIS h2_h2_$i.xyz -o $m'_'$i
+
+      qp set_file $m'_'$i
+
+      qp set electrons elec_alpha_num 1
+      qp set electrons elec_beta_num  1
+
+      # set ghost atoms
+      if [ $m == "A" ] ; then
+         qp set nuclei nucl_label  "['H', 'H', 'x', 'x']"
+         qp set nuclei nucl_charge "[1.0, 1.0, 0.0, 0.0]"
+      elif [ $m == "B" ] ; then
+         qp set nuclei nucl_label  "['x', 'x', 'H', 'H']"
+         qp set nuclei nucl_charge "[0.0, 0.0, 1.0, 1.0]"
+      fi
+
+      # Run SCF
+      qp run scf  >  $m'_'$i'.out'
+      qp run cisd >> $m'_'$i'.out'
+
+      # Export HDF5 files for GammCor
+      qp set gammcor_plugin cholesky_tolerance 1.e-5
+      qp set gammcor_plugin trexio_file \"$m.h5\"
+      qp run export_gammcor >> 'export_'$m'.out'
+      qp run gammcor_plugin >> 'export_'$m'.out'
+
+      # backup #1
+      mkdir -p results
+      mkdir -p results/$i
+      mv $m'_'$i'.out'     results/$i
+      mv $m'_'$i'.out'     results/$i
+      mv 'export_'$m'.out' results/$i
+
+   done # end QP2
+
+   export OMP_NUM_THREADS=1
+
+   # run GammCor
+   input_gammcor $i
+   $GAMMCOR_EXEC > 'gammcor_'$i'.out'
+   grepper "gammcor_"$i".out" $i
+
+   # backup #2
+   mv "gammcor_"$i".out" results/$i
+   mv A.h5  results/$i
+   mv B.h5  results/$i
+
+done
+
